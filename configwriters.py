bd61cf2494ceb74ca6ea7b220159c479a32cb241
Jamie Tanna <jamie@jamietanna.co.uk>
2014-04-21 16:32:28 +0100

    Added basis for WallpaperChangers

--- configwriters.py
+++ configwriters.py
@@ -3,6 +3,52 @@ import config
 
 from generic import *
 
+import subprocess
+
+class WallpaperChanger(object):
+    __metaclass__ = ABCMeta
+    name = ""
+
+    def __init__(self, name):
+        super(WallpaperChanger, self).__init__()
+        self.name = name
+
+    @abstractmethod
+    def changeBackground(self, path):
+        pass
+
+    def getName(self):
+        return name
+
+    # override if you want a shorter name for the command-line
+    def getShortName(self):
+        return name
+
+    def execute(self, args):
+        ret = subprocess.call(args)
+        if not ret == 0:
+            error("Some unknown error occured when changing background. ")
+
+class FehWallpaper(WallpaperChanger):
+    def __init__(self):
+        super(FehWallpaper, self).__init__("FEH")
+
+    def changeBackground(self, path):
+        self.execute(["feh", "--bg-fill", path])
+
+class GnomeWallpaper(WallpaperChanger):
+    def __init__(self):
+        super(GnomeWallpaper, self).__init__("Gnome Wallpaper Changer")
+
+    def changeBackground(self, path):
+        self.execute(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "'file://" + path + "'"])
+
+    def getShortName(self):
+        return "GnomeWP"
+
+
+
+
 class ConfigWriter(object):
     __metaclass__ = ABCMeta
     name = ""
